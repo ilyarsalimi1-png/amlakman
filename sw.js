c0dbabfcde18ce117839212ce2ff9953ee64ed53
ilyarsalimi1-png <ilyar.salimi1@gmail.com>
2026-09-13 19:35:46 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const C="amlakman-v3-20260830";const A=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="amlakman-v4";const S=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(S)))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(e.request.mode==="navigate")e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
